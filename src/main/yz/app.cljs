79842d110e5e96b2645d688767c0df18bb8169b6
Paulus Esterhazy <pesterhazy@gmail.com>
2021-02-28 19:15:09 +0100

    WIP

--- src/main/yz/app.cljs
+++ src/main/yz/app.cljs
@@ -3,38 +3,11 @@
   (:require [clojure.test :as t]
             [clojure.pprint]
             [reagent.core :as r]
-            [yz.game :as g]))
-
-(defn <die>
-  [die-val]
-  [:img.die
-   {:src (str "assets/" (if (nil? die-val) "question" die-val) ".png")}])
-
-(defn <die-set> [die-vals]
-  (->> die-vals
-       (map (fn [die-val] [<die> die-val]))
-       (into [:div])))
-
-(defonce !game (r/atom (g/make-game)))
+            [yz.view :as v]))
 
 (defn <root>
   []
-  (let [game @!game
-        can-roll? (g/can-roll? game)]
-    [:div
-     [:div
-      [<die-set> (g/die-vals game)]]
-     [:div.menu
-      [:a.menu-item.button
-       {:on-click (fn []
-                    (swap! !game g/reset))}
-       "Start"]
-      [:a.menu-item.button
-       {:class (when-not can-roll? :disabled)
-        :on-click (when can-roll?
-                    (fn []
-                      (swap! !game (fn [game] (g/roll game (g/rand-roller))))))}
-       "Roll"]]]))
+  [v/<game>])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
